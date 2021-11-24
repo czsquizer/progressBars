531413278f0114d40eaf12d949aa8be3b1de77fe
czsquizer <smrcek.marek@gmail.com>
2021-11-24 17:11:18 +0100

    Add event for starting progbar, convert __res to fxmanif

--- client.lua
+++ client.lua
@@ -6,3 +6,14 @@ function startUI(time, text)
 		text = text
 	})
 end
+
+RegisterNetEvent('progressBars:StartUI', function(time, text)
+
+	SendNUIMessage({
+	  type = "ui",
+	  display = true,
+	  time = time,
+	  text = text
+	})
+  
+  end)
